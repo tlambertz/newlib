518c09c0869dc0dd04b9734bc660d1a2b77cb37d
Stefan Lankes <stlankes@users.noreply.github.com>
2018-08-02 22:53:46 +0200

    introduce sleep function by using sys_msleep

---
+++ newlib/libc/sys/hermit/sleep.c
@@ -0,0 +1,10 @@
+/* libc/sys/hermit/sleep.c - sleep function */
+
+/* Written 2018 by Stefan Lankes */
+
+void sys_msleep(unsigned int ms)
+
+unsigned int sleep(unsigned int seconds)
+{
+  sys_msleep(1000*seconds);
+}
